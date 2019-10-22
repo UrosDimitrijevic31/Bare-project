6ac76b93f49bfecf0c7daca55f2b9c11d3b7d1ed
UrosPiksi <urosrgf@gmail.com>
2019-10-22 12:46:38 +0200

    dodat kostur

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//import '../assets/css/style.css';
+/*
+    danas smo presli osnove osnova html-a, css-a, js-a
+    sl kod nam ispisuje poruku kao i u web stranicu ubacuje neki element
+*/ 
+
+
+//nedelja 1, dan 1
+console.log('Hello by js');
+console.log('Hello world');
+
+document.getElementById('app').innerHTML = '<p>Hello to BootcampPizza by JavaScript Basics</p>';
+
+const app = document.getElementById('app');
+// app.innerHTML = '<p>Hello to BootcampPizza by JavaScript Basics</p>';
+document.getElementById('<p>hello to bootcampPizza from JavaScript!</p>');
